3b91ca824932f4f3f7556c5b55ac6dc5ff68ba59
adimogli2 <mogli.a@northeastern.edu>
2025-02-18 18:01:16 -0500

    fixed Linkedin link and added GitHub

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -95,6 +95,10 @@ const Portfolio = () => {
                   <Linkedin className="w-5 h-5 mr-2" />
                   LinkedIn
                 </a>
+                <a href={personalInfo.github} className={`flex items-center ${isDark ? 'text-gray-300 hover:text-white' : 'text-gray-600 hover:text-gray-900'}`}>
+                  <Github className="w-5 h-5 mr-2" />
+                  GitHub
+                </a>
                 <span className={`flex items-center ${isDark ? 'text-gray-300' : 'text-gray-600'}`}>
                   {personalInfo.location}
                 </span>
